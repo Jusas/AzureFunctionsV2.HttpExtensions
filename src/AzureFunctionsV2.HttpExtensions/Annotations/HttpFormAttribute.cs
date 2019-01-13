029ed3b3cfdecf72f3ec4ffbab00d79e20fbb2b0
Jussi Saarivirta <jusasi@gmail.com>
2019-01-13 18:57:25 +0200

    Set attribute multiplicity to one

--- src/AzureFunctionsV2.HttpExtensions/Annotations/HttpFormAttribute.cs
+++ src/AzureFunctionsV2.HttpExtensions/Annotations/HttpFormAttribute.cs
@@ -3,7 +3,7 @@ using Microsoft.Azure.WebJobs.Description;
 
 namespace AzureFunctionsV2.HttpExtensions.Annotations
 {
-    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = true)]
+    [AttributeUsage(AttributeTargets.Parameter, AllowMultiple = false)]
     [Binding]
     public class HttpFormAttribute : Attribute
     {
